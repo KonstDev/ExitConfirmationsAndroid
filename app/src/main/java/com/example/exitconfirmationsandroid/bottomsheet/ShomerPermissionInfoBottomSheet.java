bc2806fd8c62fdb6fcc5294e350384b9957d7b5f
Mike Maevsky <57516381+memaev@users.noreply.github.com>
2023-06-13 22:17:02 +0300

    ShomerActivity in progress

--- app/src/main/java/com/example/exitconfirmationsandroid/bottomsheet/ShomerPermissionInfoBottomSheet.java
+++ app/src/main/java/com/example/exitconfirmationsandroid/bottomsheet/ShomerPermissionInfoBottomSheet.java
@@ -8,15 +8,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.exitconfirmationsandroid.databinding.ConfirmationInfoShomerBottomSheetBinding;
+import com.example.exitconfirmationsandroid.exit_permissions.ExitPermission;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class ShomerPermissionInfoBottomSheet extends BottomSheetDialogFragment {
 
-    
+    private ExitPermission exitPermission;
+
+    public ShomerPermissionInfoBottomSheet(ExitPermission exitPermission) {
+        this.exitPermission = exitPermission;
+    }
+
+    private ConfirmationInfoShomerBottomSheetBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        binding = ConfirmationInfoShomerBottomSheetBinding.inflate(inflater, container, false);
+
+        binding.studentsNames.setText(exitPermission.students_names);
+        binding.madrichTv.setText(exitPermission.madrich_name);
+        binding.exitTime.setText(exitPermission.exitTime + " " + exitPermission.exitDate);
+
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/exitconfirmationsandroid/exit_permissions/ExitPermission.java
+++ app/src/main/java/com/example/exitconfirmationsandroid/exit_permissions/ExitPermission.java
@@ -1,8 +1,8 @@
 package com.example.exitconfirmationsandroid.exit_permissions;
 
 public class ExitPermission {
-    boolean confirmed;
-    String exitDate, exitTime, goingTo, group, madrich_name, returnDate, returnTime,
+    public boolean confirmed;
+    public String exitDate, exitTime, goingTo, group, madrich_name, returnDate, returnTime,
             students_ids, students_names;
 
     public ExitPermission(boolean confirmed, String exitDate, String exitTime, String goingTo,
